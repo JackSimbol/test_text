22e871e360b6482cec13d9d49783fed552a4dfb4
Xingbo Wang <91768006+JackSimbol@users.noreply.github.com>
2022-08-31 16:42:25 +0800

    Add files via upload

---
+++ text.js
@@ -0,0 +1,14 @@
+/* 文本题交互js
+ * tid: 题干id
+ * sid: 文本id
+ */
+
+function unparse_text(tid){
+    var sid = document.getElementById(tid).getAttribute(text_id);
+    var text = "["+document.getElementById(sid).innerHTML+"]";
+    return "text="+text;
+}
+
+document.getElementById("unparse").onclick = function(){
+    console.log(unparse_text("test_text"));
+}
